b53dade0f1056e4d40c9249dbb154d9e08d45bed
Yurii Kozhenov <yurii.kozhenov@motorolasolutions.com>
2017-09-25 15:44:20 +0300

    Continue implementing 4 lab

--- lab4/lab4.js
+++ lab4/lab4.js
@@ -2,7 +2,8 @@
   let transpositionForm = document.transpositionForm;
 
   function init() {
-    transpositionForm.input.value = 'enemyattackstonight';
+    // transpositionForm.input.value = 'enemyattackstonight';
+    transpositionForm.input.value = 'ettheakimaotycnzntsg';
     transpositionForm.key.value = '31452';
 
     transpositionForm.submit.addEventListener('click', () => {
@@ -30,6 +31,7 @@
     let temp = [];
     let output = [];
     const colunmLength = message.length / key.length;
+    const colunmLengthSquared = colunmLength * colunmLength;
 
     for (let i = 0; i < message.length; i += 5) {
       for (let j = 0; j < key.length; j++) {
@@ -37,9 +39,11 @@
       }
     }
 
-    for (let i = 0; i < key.length; i++) {
-      for (let j = 0; j < colunmLength; j++) {
-        output.push(temp[j][i]);
+    const it = iterator(temp);
+
+    for (let i = 0; i < colunmLength; i++) {
+      for (let j = 0; j <= colunmLengthSquared; j += 4) {
+        output[i + j] = it.next().value;
       }
     }
 
@@ -47,9 +51,37 @@
   }
 
   function transpositionCipherDencrypt(key, message) {
+    let temp = [];
     let output = [];
+    const it = iterator(message);
+    const colunmLengthSquared = (message.length / key.length) ** 2;
+
+    for (let i = 0; i < key.length; i++) {
+      for (let j = 0; j <= colunmLengthSquared; j += 5) {
+        temp[i + j] = it.next().value;
+      }
+    }
+
+    for (let i = 0; i < message.length; i += 5) {
+      for (let j = 0; j < key.length; j++) {
+        console.log(i + key[j], message[i + key[j]]);
+        output.push(message[i + key[j]]);
+      }
+    }
 
     return output.join('');
+  }
+
+  function iterator(array) {
+    let nextIndex = 0;
+
+    return {
+      next: function() {
+        return nextIndex < array.length ?
+          { value: array[nextIndex++], done: false } :
+          { done: true };
+      }
+    };
   }
 
   init();
